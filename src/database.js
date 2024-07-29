b9ce8a61bf428f345f7dfb0cc315f2d65247574b
mrinalummadising <mrinalummadising@gmail.com>
2024-07-29 12:14:20 +0530

    Artists Fontend changes

---
+++ src/database.js
@@ -0,0 +1,46 @@
+const transactions = [
+  {
+    date: "2020-02-20",
+    type: "Debit",
+    amount: 3000,
+    description: "Misc Expenses",
+  },
+  {
+    date: "2020-02-18",
+    type: "Debit",
+    amount: 285,
+    description: "Printing sheets for office documents",
+  },
+  {
+    date: "2020-02-18",
+    type: "Debit",
+    amount: 500,
+    description: "Snacks Party",
+  },
+  {
+    date: "2020-02-17",
+    type: "Credit",
+    amount: 5000,
+    description: "Credited to Office Account",
+  },
+  {
+    date: "2020-02-21",
+    type: "Credit",
+    amount: 7000,
+    description: "Credited to Office Account",
+  },
+  {
+    date: "2020-02-26",
+    type: "Dedit",
+    amount: 15000,
+    description: "Credited to Office Account",
+  },
+  {
+    date: "2020-02-28",
+    type: "Dedit",
+    amount: 20000,
+    description: "Credited to Office Account",
+  },
+];
+
+export default transactions;
